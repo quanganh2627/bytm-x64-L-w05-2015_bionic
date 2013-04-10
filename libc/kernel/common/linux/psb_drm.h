1c026f98bad80978039731af979d802383eb1bd0
Li Zeng <li.zeng@intel.com>
2013-04-10 08:20:16 +0800

    psb_drm: modify psb_drm.h
    BZ: 92956
    
    modify psb_drm.h
    
    Change-Id: I5cb786a2742aae72dc50a2e7535a3a0f99826b1a
    Signed-off-by: Li Zeng <li.zeng@intel.com>
    Reviewed-on: http://android.intel.com:8080/100620
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Ma, Hongpeng <hongpeng.ma@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- libc/kernel/common/linux/psb_drm.h
+++ libc/kernel/common/linux/psb_drm.h
@@ -163,6 +163,7 @@ struct psb_validate_req {
  uint32_t group;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  uint32_t pad64;
+ uint32_t unfence_flag;
 };
 struct psb_validate_rep {
  uint64_t gpu_offset;
