8d2559463d93f5ffb9e97811beeb827f04884b70
Nick Kralevich <nnk@google.com>
2013-04-19 16:54:22 -0700

    [BACKPORT FROM GOOGLE] libc: cleanup strchr
    BZ:114474
    
    Move strchr to a .cpp file, and change to bionic directory.
    
    Change-Id: I64ade7df326c0a9a714aca4caf5647b6833b1c97

--- libc/Android.mk
+++ libc/Android.mk
@@ -97,7 +97,6 @@ libc_common_src_files := \
 	string/index.c \
 	string/strcasecmp.c \
 	string/strcat.c \
-	string/strchr.c \
 	string/strcspn.c \
 	string/strdup.c \
 	string/strlcat.c \
@@ -270,6 +269,7 @@ libc_bionic_src_files := \
     bionic/signalfd.cpp \
     bionic/sigwait.cpp \
     bionic/__strcat_chk.cpp \
+    bionic/strchr.cpp \
     bionic/__strcpy_chk.cpp \
     bionic/strerror.cpp \
     bionic/strerror_r.cpp \
