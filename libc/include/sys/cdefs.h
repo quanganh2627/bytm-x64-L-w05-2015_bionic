151432417a324049058b8b1e291854400c14c62f
Du Chenyang <chenyang.du@intel.com>
2013-06-08 11:25:10 +0800

    [PDK MR2 AOSP] Replace inline with __inline__ in macro __BIONIC_FORTIFY_INLINE
    BZ: 114804
    
    umask multi-define error will occur in external/pcsc when try to
    enable fortify. Replace inline with __inline__ to fix it from bionic.
    
    Change-Id: I428babc9dd87d7ad424268e7e3120e2a075b9574
    Signed-off-by: Du Chenyang <chenyang.du@intel.com>

--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -524,7 +524,7 @@
 #endif
 
 #define __BIONIC_FORTIFY_INLINE \
-    extern inline \
+    extern __inline__ \
     __attribute__ ((always_inline)) \
     __attribute__ ((gnu_inline)) \
     __attribute__ ((artificial))
