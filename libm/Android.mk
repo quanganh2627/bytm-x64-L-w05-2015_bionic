aosp-mirror/platform_bionic

Add a flag control that PRIVATE libm can be chosen
Change-Id: I080456a2c45ac870965c25552c5821875568fe9b
Author: Xiaokang, Qin <xiaokang.qin@intel.com>
Signed-off-by: Xiaokang, Qin <xiaokang.qin@intel.com>
Signed-off-by: Shuo Gao <shuo.gao@intel.com>
Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
Signed-off-by: Jack Ren <jack.ren@intel.com>
Author-tracking-BZ: 63396

libm/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(TARGET_USE_PRIVATE_LIBM),true)
 LOCAL_PATH:= $(call my-dir)
 
 libm_common_src_files:= \
@@ -227,3 +228,4 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 
 include $(BUILD_SHARED_LIBRARY)
+endif

quanganh2627/bytm-x64-L-w05-2015_bionic

Bug: 63396
Category: Device Enablement
Domain: AOSP-bionic
Severity: Medium
Origin: Internal
Upstream-candidate: No
Change-Id: Ieb333c5b8b58e26b5da4f5b40d22a3fc13eb5cd5
Orig-Change-Id: Icb4c07ed1e8c76f1673c0b74efaaa5158842f525
Reviewed-on: http://android.intel.com:8080/74475
Reviewed-by: Yao, Yong <yong.yao@intel.com>
Reviewed-by: lab_cactus <lab_cactus@intel.com>
Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>

libm/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(USE_PRIVATE_LIBM),true)
 LOCAL_PATH:= $(call my-dir)
 
 # TODO: these come from from upstream's libc, not libm!
@@ -252,3 +253,4 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_WHOLE_STATIC_LIBRARIES := libm
 include $(BUILD_SHARED_LIBRARY)
+endif