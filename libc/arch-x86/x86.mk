5d19749c625f2dd9e55ed214208c06dbda301378
Varvara Rainchik <varvara.rainchik@intel.com>
2014-04-21 16:07:52 +0400

    Add SLM-tuned strcpy, strncpy, stpcpy, stpncpy, strlen, memcmp and wmemcmp. Delete an old version of strlen. Create new directories inside arch-x86 to specify architecture: x86-atom, x86-slm and generic (non atom or silvermont architectures are treated like generic). Due to introducing optimized versions of stpcpy and stpncpy, c-implementations of these functions are moved from common for architectures makefile to arm and mips specific makefiles.
    BZ: 190400
    
    Submitted versions are at least 5% faster on unit tests on MERR.
    All changes were merged into AOSP.
    
    Change-Id: Iff20fa2dc6f2d2a3a2d71140c05b0f925f1147ec
    Category: device enablement
    Domain: AOSP-Bionic
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Varvara Rainchik <varvara.rainchik@intel.com>

--- libc/arch-x86/x86.mk
+++ libc/arch-x86/x86.mk
@@ -11,87 +11,16 @@ _LIBC_ARCH_COMMON_SRC_FILES := \
     arch-x86/bionic/sigsetjmp.S \
     arch-x86/bionic/syscall.S \
     arch-x86/bionic/vfork.S \
-    arch-x86/string/ffs.S
+    arch-x86/generic/string/ffs.S \
 
-ifeq ($(TARGET_ARCH_VARIANT), x86-atom)
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/ssse3-memcpy-atom.S \
-	arch-x86/string/ssse3-memmove-atom.S \
-	arch-x86/string/ssse3-bcopy-atom.S \
-	arch-x86/string/sse2-memset-atom.S \
-	arch-x86/string/sse2-bzero-atom.S
-else
-# Fall here for slm. Also those functions are faster for hsw/ivy/sandy,
-# so we are using them as the default ones.
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/sse2-memcpy-slm.S \
-	arch-x86/string/sse2-memmove-slm.S \
-	arch-x86/string/sse2-bcopy-slm.S \
-	arch-x86/string/sse2-memset-slm.S \
-	arch-x86/string/sse2-bzero-slm.S
+arch_variant_mk := $(LOCAL_PATH)/arch-x86/$(TARGET_ARCH_VARIANT)/$(TARGET_ARCH_VARIANT).mk
+ifeq ($(wildcard $(arch_variant_mk)),)
+    arch_variant_mk := $(LOCAL_PATH)/arch-x86/generic/generic.mk
 endif
+include $(arch_variant_mk)
+libc_common_additional_dependencies += $(arch_variant_mk)
 
-ifeq ($(ARCH_X86_HAVE_SSSE3),true)
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/ssse3-strncat-atom.S \
-	arch-x86/string/ssse3-strncpy-atom.S \
-	arch-x86/string/ssse3-strlcat-atom.S \
-	arch-x86/string/ssse3-strlcpy-atom.S \
-	arch-x86/string/ssse3-strcmp-atom.S \
-	arch-x86/string/ssse3-strncmp-atom.S \
-	arch-x86/string/ssse3-strcat-atom.S \
-	arch-x86/string/ssse3-strcpy-atom.S \
-	arch-x86/string/ssse3-memcmp-atom.S \
-	arch-x86/string/ssse3-wmemcmp-atom.S \
-	arch-x86/string/ssse3-memcmp16-atom.S \
-	arch-x86/string/ssse3-wcscat-atom.S \
-	arch-x86/string/ssse3-wcscpy-atom.S
-else
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/strcmp.S \
-	arch-x86/string/strncmp.S \
-	arch-x86/string/strcat.S \
-	arch-x86/string/memcmp.S \
-	string/memcmp16.c \
-	string/strcpy.c \
-	string/strncat.c \
-	string/strncpy.c \
-	string/strlcat.c \
-	string/strlcpy.c \
-	upstream-freebsd/lib/libc/string/wcscpy.c \
-	upstream-freebsd/lib/libc/string/wcscat.c \
-	upstream-freebsd/lib/libc/string/wmemcmp.c
-endif
-
-ifeq ($(ARCH_X86_HAVE_SSE2),true)
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/sse2-memchr-atom.S \
-	arch-x86/string/sse2-memrchr-atom.S \
-	arch-x86/string/sse2-strchr-atom.S \
-	arch-x86/string/sse2-strrchr-atom.S \
-	arch-x86/string/sse2-index-atom.S \
-	arch-x86/string/sse2-strlen-atom.S \
-	arch-x86/string/sse2-strnlen-atom.S \
-	arch-x86/string/sse2-wcschr-atom.S \
-	arch-x86/string/sse2-wcsrchr-atom.S \
-	arch-x86/string/sse2-wcslen-atom.S \
-	arch-x86/string/sse2-wcscmp-atom.S
-else
-_LIBC_ARCH_COMMON_SRC_FILES += \
-	arch-x86/string/memset.S \
-	arch-x86/string/strlen.S \
-	arch-x86/string/bzero.S \
-	bionic/memrchr.c \
-	bionic/memchr.c \
-	string/strchr.cpp \
-	string/strrchr.c \
-	string/index.c \
-	bionic/strnlen.c \
-	upstream-freebsd/lib/libc/string/wcschr.c \
-	upstream-freebsd/lib/libc/string/wcsrchr.c \
-	upstream-freebsd/lib/libc/string/wcslen.c \
-	upstream-freebsd/lib/libc/string/wcscmp.c
-endif
+arch_variant_mk :=
 
 _LIBC_ARCH_STATIC_SRC_FILES := \
     bionic/dl_iterate_phdr_static.c \
